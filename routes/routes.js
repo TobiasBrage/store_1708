ada9f13d6d07789794a86d3985ae39edea5a8d04
Tobias Brage <gamerfreaq@live.com>
2018-08-20 08:53:59 +0200

    db relationer

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,24 @@
 module.exports = function (app) {
 
     app.get('/', function (req, res) {
-        let sql = `SELECT products.title, 
+       let sql = `SELECT products.title,
         products.image, 
         products.description, 
         products.price, 
         products.created_unix as "unix", 
-        products_category.category_name as "category"
-        FROM products_category
-        INNER JOIN products ON products_category.id = products.category_fk WHERE products.active = 1;`;
+        products_category.category_name as "category",
+        products_manufacturer.name as "manufacturer"
+        FROM products
+        INNER JOIN products_category ON products.category_fk = products_category.id
+        INNER JOIN products_manufacturer ON products.manufacturer_fk = products_manufacturer.id
+        WHERE products.active = 1;`;
 
-        // let sqlDoubleRelation = `SELECT * FROM produkt_og_billede
+        // let sqlDoubleRelation = `SELECT * FROM produkt
         // INNER JOIN produkt ON fk_produkt_id = produkt_id
         // INNER JOIN billede ON fk_billede_id = billede_id;`;
     
         db.query(sql, function (err, result) {
+            console.log(result);
             res.send(result);
         })
     });
